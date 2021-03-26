da04a243516a9120eff64e19762e9a5f48ac2d14
a07fgchen <47937853+a07fgchen@users.noreply.github.com>
2021-03-26 13:17:20 +0800

    factory

---
+++ Factory.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('display_errors','1');
+error_reporting(E_ALL);
+
+include_once './CacheMysql.php';
+
+$setting['mysql']['host'] = '127.0.0.1';
+$setting['mysql']['dbname'] = 'test';
+$setting['mysql']['charset'] = 'UTF8';
+$setting['mysql']['user'] = 'shieldon';
+$setting['mysql']['pass'] = 'taiwan';
+$setting['mysql']['table'] = 'cache_data';
+
+$setting['mysql']['host'] = '127.0.0.1';
+$setting['mysql']['port'] = '6379';
+
+$factory = new CacheFactory();
+
+$cache = $factory->createDriver('mysql',$setting);
+
+$cache->set('hello','world');
+echo $cache->get('hello');
